96a39749fc9d99470a62ea5ebc53c39237552007
Stef <stef.robbe@gmail.com>
2022-03-27 14:52:43 +0200

    added button properly-ish

--- content.js
+++ content.js
@@ -1,25 +1,18 @@
-// sleep to wait for video playback
-// const sleep = (ms) => {
-//     return new Promise(resolve => setTimeout(resolve, ms))
-// }
-// await sleep(3000)
-
-//container-lg
-//- remove margin-left auto
-//
-//inset-video-item-container mt4
-//- add width 203, height 120
-//
-//col-xl-10 offset-xl-1
-//- remove margin-left 8.3333
-//
-//inset-video-item-image-container position-relative d-flex
-//- remove pos relative
-//
-
 let isTheaterMode = false;
 
 function toggleTheater() {
+    //container-lg
+    //- remove margin-left auto
+    
+    //inset-video-item-container mt4
+    //- add width 203, height 120
+    
+    //col-xl-10 offset-xl-1
+    //- remove margin-left 8.3333
+
+    //inset-video-item-image-container position-relative d-flex
+    //- remove pos relative
+
     const containerlg = document.getElementsByClassName("vod-detail-page")[0].firstChild;
     const insetvideoitemcontainer = document.getElementsByClassName("inset-video-item-container")[0];
     const offset = document.getElementsByClassName("col-xl-10 offset-xl-1")[0];
@@ -38,12 +31,25 @@ function toggleTheater() {
     isTheaterMode = !isTheaterMode;
 }
 
-const fullscBtn = document.getElementById("bmpui-id-76");
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-const clone = fullscBtn.cloneNode(true);
+async function addTheaterButton() {
+    var fullscBtn;
+    for (let i = 0; i < 10; i++) {
+        await sleep(i * 1000);
+        fullscBtn = document.getElementById("bmpui-id-76");
+        if (fullscBtn != null) break;
+    }
 
-clone.id = "f1tv-upgrade-theater";
-clone.setAttribute('title', 'Theater Mode');
-clone.setAttribute('aria-label', 'Theater Mode');
-clone.addEventListener("click", toggleTheater);
-fullscBtn.before(clone);
+    const clone = fullscBtn.cloneNode(true);
+    
+    clone.id = "f1tv-upgrade-theater";
+    clone.setAttribute('title', 'Theater Mode');
+    clone.setAttribute('aria-label', 'Theater Mode');
+    clone.addEventListener("click", toggleTheater);
+    fullscBtn.before(clone);
+}
+
+addTheaterButton();
